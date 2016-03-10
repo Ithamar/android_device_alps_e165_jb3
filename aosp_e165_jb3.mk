a5d6f9e4ce6b6ee8c7b3dbd8668af623191d44ba
Ithamar R. Adema <ithamar@upgrade-android.com>
2016-03-10 17:23:35 +0100

    add 4.2 compatibility

--- aosp_e165_jb3.mk
+++ aosp_e165_jb3.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += \
 #PRODUCT_COPY_FILES := device/lge/mako/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 PRODUCT_NAME := aosp_e165_jb3
 PRODUCT_DEVICE := e165_jb3
